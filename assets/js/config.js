3a059675137e0a52a8e142274ced4c934da25e3f
Ayush Kumar Yadav <tech.ayush1400@gmail.com>
2026-04-09 17:51:23 +0530

    Added config file

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+
+const EMAILJS_CONFIG = {
+  publicKey:         "VFIq0REc2ihtHEet-",   
+  serviceId:         "service_3jc40sm",  
+  notificationTplId: "template_nierde8",
+  autoReplyTplId:    "template_vay8nh8",
+};  
